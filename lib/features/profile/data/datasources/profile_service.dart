bf301dbf9c0531f8dc7835b459ca57e344d27ce8
DiegoR434 <u202123548@upc.edu.pe>
2025-07-11 02:09:13 -0500

    feat: added v1 profile service

---
+++ lib/features/profile/data/datasources/profile_service.dart
@@ -0,0 +1,41 @@
+import 'dart:convert';
+import 'dart:io';
+
+import 'package:http/http.dart' as http;
+import 'package:profile_page/features/profile/data/models/driver_dto.dart';
+import 'package:profile_page/features/profile/domain/driver.dart';
+
+class ProfileService {
+
+  Future<Driver> getDriver(int id) async{
+    final Uri url = Uri.parse('http://localhost:3000/users/${id}');
+    http.Response response = await http.get(url);
+    if (response.statusCode == HttpStatus.ok) {
+      final drivermap = jsonDecode(response.body) as Map<String, dynamic>;
+      return DriverDto.fromJson(drivermap).toDomain();
+    } else {
+      throw Exception('Failed to load driver');
+    }
+  
+  }
+
+  Future<void> updateDriver(Driver driver) async {
+    final Uri url = Uri.parse('http://localhost:3000/users/${driver.id}');
+    final response = await http.put(
+      url,
+      headers: {'Content-Type': 'application/json'},
+      body: jsonEncode({
+        'id': driver.id,
+        'name': driver.name,
+        'mail': driver.mail,
+        'university': driver.university,
+        'car': driver.car,
+      }),
+    );
+
+    if (response.statusCode != HttpStatus.ok) {
+      throw Exception('Failed to update driver');
+    }
+  }
+
+}
