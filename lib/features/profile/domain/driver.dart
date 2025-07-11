6c5bb4f5ad55f878f76ad373ab796c6691b3fe36
DiegoR434 <u202123548@upc.edu.pe>
2025-07-11 01:57:49 -0500

    feat: added domain driver entitie

---
+++ lib/features/profile/domain/driver.dart
@@ -0,0 +1,16 @@
+class Driver {
+  final int id; 
+  final String name;
+  final String mail;
+  final String university;
+  final String car;
+
+  Driver({
+    required this.id,
+    required this.name,
+    required this.mail,
+    required this.university,
+    required this.car,
+  });
+  
+}
