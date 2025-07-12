0f5b432bca346e02694fd559be08cf3e3c9d6fdd
DiegoR434 <u202123548@upc.edu.pe>
2025-07-11 19:50:08 -0500

    refactor: refactor getDriver and login methods to use Dio for API requests

--- lib/features/profile/data/datasources/profile_service.dart
+++ lib/features/profile/data/datasources/profile_service.dart
@@ -1,21 +1,39 @@
 import 'dart:convert';
 import 'dart:io';
 
+import 'package:flutter/cupertino.dart';
 import 'package:http/http.dart' as http;
 import 'package:profile_page/features/profile/data/models/driver_dto.dart';
 import 'package:profile_page/features/profile/domain/driver.dart';
+import 'package:profile_page/features/shared/service/api_client.dart';
 
 class ProfileService {
 
-  Future<Driver> getDriver(int id) async{
-    final Uri url = Uri.parse('https://movbackfinallll-production.up.railway.app/auth/me');
-    http.Response response = await http.get(url);
-    if (response.statusCode == HttpStatus.ok) {
-      final drivermap = jsonDecode(response.body) as Map<String, dynamic>;
-      return DriverDto.fromJson(drivermap).toDomain();
-    } else {
-      throw Exception('Failed to load driver');
-    }
+  Future<Driver> getDriver() async{
+    final dio = ApiClient().dio;
+
+
+     try {
+    final response = await dio.get(
+      'https://movbackfinallll-production.up.railway.app/auth/me',
+    );
+
+       return DriverDto.fromJson(response.data as Map<String, dynamic>).toDomain();
+  } catch (e) {
+    throw Exception('Error al obtener datos del conductor');
+  }
+
+
+    // final Uri url = Uri.parse('https://movbackfinallll-production.up.railway.app/auth/me');
+    // http.Response response = await http.get(url);
+    // print("as;dlkfjas;ldfkjasd;lkfjsad;lfkjasdlfkasjdklfsdjlkfljkfajlk--------");
+    // print(response.body);
+    // if (response.statusCode == HttpStatus.ok) {
+    //   final drivermap = jsonDecode(response.body) as Map<String, dynamic>;
+    //   return DriverDto.fromJson(drivermap).toDomain();
+    // } else {
+    //   throw Exception('Failed to load driver');
+    // }
   
   }
 
--- lib/features/security/data/datasource/user_login_service.dart
+++ lib/features/security/data/datasource/user_login_service.dart
@@ -1,34 +1,47 @@
 import 'dart:convert';
 import 'dart:io';
 import 'package:http/http.dart' as http;
+import 'package:profile_page/features/shared/service/api_client.dart';
 
 class UserLoginService {
   Future<bool> login(String correo, String contrasena) async {
-    final Uri url = Uri.parse('https://movbackfinallll-production.up.railway.app/auth/login');
+    final dio = ApiClient().dio;
 
-    final response = await http.post(
-      url,
-      headers: {
-        'Content-Type': 'application/json',
-      },
-      body: jsonEncode({
-        'correo': correo,
-        'contrasena': contrasena,
-      }),
+ try {
+    final response = await dio.post(
+      'https://movbackfinallll-production.up.railway.app/auth/login',
+      data: {'correo': correo, 'contrasena': contrasena},
     );
+    return response.data['success'] == true;
+  } catch (e) {
+    return false;
+  }
 
-    // Imprimir para verificar respuesta del backend
-    print('STATUS: ${response.statusCode}');
-    print('BODY: ${response.body}');
+    // final Uri url = Uri.parse('https://movbackfinallll-production.up.railway.app/auth/login');
 
-    if (response.statusCode != HttpStatus.ok) {
-      throw Exception('Error al iniciar sesión');
-    }
+    // final response = await http.post(
+    //   url,
+    //   headers: {
+    //     'Content-Type': 'application/json',
+    //   },
+    //   body: jsonEncode({
+    //     'correo': correo,
+    //     'contrasena': contrasena,
+    //   }),
+    // );
 
-    final data = jsonDecode(response.body);
+    // // Imprimir para verificar respuesta del backend
+    // print('STATUS: ${response.statusCode}');
+    // print('BODY: ${response.body}');
 
-    // Aquí puedes ajustar la lógica dependiendo de lo que devuelva el backend.
-    // Por ejemplo, si devuelve {"success": true}, podrías usar eso:
-    return data['success'] == true;
+    // if (response.statusCode != HttpStatus.ok) {
+    //   throw Exception('Error al iniciar sesión');
+    // }
+
+    // final data = jsonDecode(response.body);
+
+    // // Aquí puedes ajustar la lógica dependiendo de lo que devuelva el backend.
+    // // Por ejemplo, si devuelve {"success": true}, podrías usar eso:
+    // return data['success'] == true;
   }
 }
